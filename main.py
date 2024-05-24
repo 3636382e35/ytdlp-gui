fee50750609aad95c918491770b4245420177c46
3636382e35 <jtuyay10@gmail.com>
2024-05-24 23:59:33 +0800

    .

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic, QtCore, QtGui
 
 
-
 class YoutubeDownload(QThread):
 
     progress = pyqtSignal(int)
     message = pyqtSignal(str, str)  # Format and message
+    
 
 
     def __init__(self, url, ydl_opts, *args, **kwargs):
@@ -29,7 +29,6 @@ class YoutubeDownload(QThread):
         self.ydl_opts = MainWindow.ydl_opts
         
 
-
     def run(self):
         def my_hook(d):
             if d['status'] == 'downloading' and d.get('total_bytes'):
@@ -37,18 +36,23 @@ class YoutubeDownload(QThread):
                 print(progress)
                 self.progress.emit(progress)
             if d['status'] == 'finished':
-                self.message.emit('<span style="color:green;">{}</span>', "Download completed successfully.")
+                self.message.emit(
+                    '<span style="color:green;">{}</span>', 
+                    "Download completed successfully."
+                )
+                MainWindow.update_progress(0)
 
         self.ydl_opts['progress_hooks'] = [my_hook]
 
         with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
             try:
                 ydl.download([self.url])
-                self.message.emit('<span style="color:green;">{}</span>', "Download completed successfully.")
+                # self.message.emit('<span style="color:green;">{}</span>', "Download completed successfully.")
             except Exception as e:
-                self.message.emit('<span style="color:red;">{}</span>', f"Error: {str(e)}")
-
-
+                self.message.emit(
+                    '<span style="color:red;">{}</span>', 
+                    f"Error: {str(e)}"
+                )
                 
     def stop(self):
         self.terminate()
@@ -72,9 +76,9 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('./ui/main_v2.ui', self)
 
-        self.errorFormat = '<span style="color:red;">{}</span>'
-        self.warningFormat = '<span style="color:orange;">{}</span>'
-        self.validFormat = '<span style="color:green;">{}</span>'
+        self.errorFormat = '<span style="color:#fb4934;">{}</span>'
+        self.warningFormat = '<span style="color:#fabd2f;">{}</span>'
+        self.validFormat = '<span style="color:#b8bb26;">{}</span>'
 
         # buttons
         self.format_audio_Rbtn.toggled.connect(lambda: self.ydl_opts.update({
@@ -85,12 +89,11 @@ class MainWindow(QMainWindow):
                 }]
         }))
 
-        self.format_video_Rbtn.toggled.connect(lambda: self.ydl_opts.update({'format': 'bestvideo/best'}))
-        self.clear_edit_txt_btn.clicked.connect(self.textEdit.clear)
-        self.ydl_opts_btn.clicked.connect(self.check_ydl_opts)
-        self.start_download_button.clicked.connect(self.onEditingFinished)
-        self.check_url_button.clicked.connect(self.on_check_url_click)
-        self.yt_search_chkbx.stateChanged.connect(lambda: self.ydl_opts.update({'yt_search': self.yt_search_chkbx.isChecked()}))
+        self.yt_search_chkbx.stateChanged.connect(
+            lambda: self.ydl_opts.update({
+                'yt_search': self.yt_search_chkbx.isChecked()
+            })
+        )
 
 
 
